79412293a818a91e7ce252d19353be36c9f4e2ab
George Benjamin <msdcconnect@gmail.com>
2018-12-19 14:54:36 +0100

    Cached query for a single menu item with Redis, hopefullt it works

--- models/menu.js
+++ models/menu.js
@@ -45,3 +45,27 @@ module.exports.deleteMenuItem = (name, callback) => {
         name,
     }, callback)
 }
+
+module.exports.findOneMenuItem = (redis, name, callback) => {
+    /*Where redis is a reference to redis, name is the name of the food */
+    redis.get(name, (err, response) => {
+        if(err){
+            callback(null)
+        }else if(response) {
+            callback(JSON.parse(response))
+        }else{
+            MenuModel.findOne({
+                name,
+            }, (err, data) => {
+                if(err) {
+                    console.log('Not found in the db');
+                    callback(null)
+                }else{
+                    redis.set(`Menu item: ${name}`, JSON.stringify(data), () => {
+                        callback(data)
+                    })
+                }
+            })
+        }
+    })
+}
