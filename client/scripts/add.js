b8935bfeffaeaf87d39afb959fa22fbce7c05679
rasheeddavid <griffinc317@gmail.com>
2018-12-21 13:37:50 +0100

    add.js

--- client/scripts/add.js
+++ client/scripts/add.js
@@ -1,34 +1,44 @@
-var submitBtn = document.getElementById('addDishBtn');
-var foodName = document.getElementById('mName');
-var desc = document.getElementById('mDesc');
-var cat = document.getElementById('mCat');
-var price = document.getElementById('mPrice');
-var img = document.getElementById('mImg');
+const API_URL = "http://localhost:3000/dashboard";
+var submitBtn = document.getElementById("addDishBtn");
+var foodName = document.getElementById("mName");
+var desc = document.getElementById("mDesc");
+var cat = document.getElementById("mCat");
+var price = document.getElementById("mPrice");
+var img = document.getElementById("mImg");
 
-var selectedFile
-img.onchange = function(){
-    selectedFile = img.files;
-    console.log(selectedFile)
-}
+var selectedFile;
+img.onchange = function() {
+  selectedFile = img.files;
+  console.log(selectedFile);
+};
 
+submitBtn.addEventListener("click", e => {
+  e.preventDefault();
+  console.log(selectedFile[0]);
+  console.log("Clicked");
 
-submitBtn.addEventListener('click', (e)=> {
-    e.preventDefault();
-    console.log(selectedFile[0])
-    console.log('Clicked')
-    var formData = new FormData();
-    console.log(foodName.value, desc.value, cat.value, price.value);
-    formData.append("name", foodName.value);
-    formData.append("description", desc.value);
-    formData.append("category", cat.value);
-    formData.append("price", price.value);
-    formData.append('img', selectedFile[0].uri);
+  const data = {
+    name: foodName.value,
+    description: desc.value,
+    category: cat.value,
+    price: price.value,
+    img: selectedFile[0].uri
+  };
 
-    fetch( 'http://localhost:3000/dashboard', {
-        method: 'POST',
-        body: formData
-       })
-       .then((res) => res.json())
-       .then((json) => console.log(json))
-       .catch(err => console.log(err));
-})
+  // console.log(data); check the stucture before you push to the api
+  // console.log(JSON.stringify(data))
+
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      // return result;
+    })
+    .catch(err => console.log(err));
+});
