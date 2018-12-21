4351bb55c90ba0bb31801ad3390f4c7fc7021f85
rasheeddavid <griffinc317@gmail.com>
2018-12-21 10:04:32 +0100

    saving_test

---
+++ tests/saving_test.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const newDish = require("../models"); // models yet
+
+// Describe our tests
+describe("Saving records", function() {
+  // Create tests
+  it("Saves a record to the database", function(done) {
+    const dish = new newDish({
+      name: "shrimp",
+      description: "fresh and well seasoned shrimp",
+      category: "sea-food",
+      price: 10
+    });
+
+    dish.save().then(function() {
+      assert(!char.isNew);
+      done();
+    });
+  });
+});
